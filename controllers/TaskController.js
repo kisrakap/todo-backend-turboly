d2d19855de0aba99f68ff29c83f1df96ee7eb626
kisrakap <kisrakap@gmail.com>
2026-05-21 16:12:29 +0700

    adding some change in update task dan delete task

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -15,9 +15,11 @@ exports.createTask = async (req, res) => {
       userId,
     });
 
-    res.status(201).json({task : task , message: "Task created successfully" }  );
+    res.status(201).json({ task: task, message: "Task created successfully" });
   } catch (err) {
-    res.status(500).json({ message: "Gagal membuat task", error: "Failed to create task" });
+    res
+      .status(500)
+      .json({ message: "Gagal membuat task", error: "Failed to create task" });
   }
 };
 
@@ -27,56 +29,80 @@ exports.getTasks = async (req, res) => {
     if (!userId) {
       return res.status(400).json({ error: "Missing userId query parameter" });
     }
-    const tasks = await Task.findAll({ where: { userId }, order: [['createdAt', 'DESC']] });
+    const tasks = await Task.findAll({
+      where: { userId },
+      order: [["createdAt", "DESC"]],
+    });
     res.json(tasks);
     res.status(200).json({ message: "Tasks fetched successfully" });
   } catch (err) {
-    res.status(500).json({ message: "Gagal mengambil task", error: "Failed to fetch tasks" });
+    res.status(500).json({
+      message: "Gagal mengambil task",
+      error: "Failed to fetch tasks",
+    });
   }
-};  
+};
 
 exports.updateTask = async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const { title, description, dueDate, priority, isComplete } = req.body;
-        const task = await Task.findByPk(taskId);
-        if (!task) {
-            return res.status(404).json({ message: "Task Tidak Ditemukan" });
-        }
-        await task.update({ title, description, dueDate, priority, isComplete });
-        tes.status(200).json({ task, message: "Task updated successfully" });
-    } catch (err) { 
-        res.status(500).json({ message: "Gagal memperbarui task", error: "Failed to update task" });
+  try {
+    const taskId = req.params.id;
+    const { title, description, dueDate, priority, isComplete } = req.body;
+    const task = await Task.findByPk(taskId);
+    if (!task) {
+      return res.status(404).json({ message: "Task Tidak Ditemukan" });
     }
+    await task.update({
+      title: title || task.title,
+      description: description || task.description,
+      dueDate: dueDate || task.dueDate,
+      priority: priority || task.priority,
+      isComplete: isComplete !== undefined ? isComplete : task.isComplete,
+    });
+
+    res.status(200).json({ message: "Tugas berhasil diperbarui", task });
+  } catch (err) {
+    res.status(500).json({
+      message: "Gagal memperbarui task",
+      error: "Failed to update task",
+    });
+  }
 };
 
 exports.toggleComplete = async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const task = await Task.findByPk(taskId);
-        if (!task) {
-            return res.status(404).json({ message: "Task Tidak Ditemukan" });
-        }
-        task.isComplete = !task.isComplete;
-        await task.save();
-        res.status(200).json({ task, message: "Task completion toggled successfully" });    
-    } catch (err) {
-        res.status(500).json({ message: "Gagal memperbarui status task", error: "Failed to toggle task completion" });
+  try {
+    const taskId = req.params.id;
+    const task = await Task.findByPk(taskId);
+    if (!task) {
+      return res.status(404).json({ message: "Task Tidak Ditemukan" });
     }
-
+    task.isComplete = !task.isComplete;
+    await task.save();
+    res
+      .status(200)
+      .json({ task, message: "Task completion toggled successfully" });
+  } catch (err) {
+    res.status(500).json({
+      message: "Gagal memperbarui status task",
+      error: "Failed to toggle task completion",
+    });
+  }
 };
 
 exports.deleteTask = async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const task = await Task.findByPk(taskId);
-        if (!task) {
-            return res.status(404).json({ message: "Task Tidak Ditemukan" });
-        }
-        await task.destroy();
-        res.status(200).json({ message: "Task deleted successfully" });
-    } catch (err) {
-        res.status(500).json({ message: "Gagal menghapus task", error: "Failed to delete task" });
+  try {
+    const taskId = req.params.id;
+    const task = await Task.findByPk(taskId);
+    if (!task) {
+      return res.status(404).json({ message: "Task Tidak Ditemukan" });
     }
+    await task.destroy();
+    res
+      .status(200)
+      .json({ message: `Task ${task.title} deleted successfully` });
+  } catch (err) {
+    res.status(500).json({
+      message: "Gagal menghapus task",
+      error: "Failed to delete task",
+    });
+  }
 };
-
