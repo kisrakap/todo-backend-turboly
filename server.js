16cd794c42696eca4c43f50c05a19ffcfada5ddc
kisrakap <kisrakap@gmail.com>
2026-05-22 09:00:41 +0700

    Fix CORS to allow localhost:8000 for web login tests

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3001",
   "http://localhost:3000",
+  "http://localhost:8000",
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
